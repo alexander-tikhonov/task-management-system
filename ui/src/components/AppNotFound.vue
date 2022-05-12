7e4843b9c1ac9a88c19b75df83f4ab865cf94fad
Тихонов Александр Дмитриевич <a.tikhonov95@gmail.com>
2022-05-12 20:07:35 +0300

    Added 404

---
+++ ui/src/components/AppNotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-full">
+    <h1>Не найдено</h1>
+    <p>
+      Запрашиваемая страница не найдена. Перейдите
+      <router-link :to="{name: 'TasksAssignee'}">
+        на главную
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppNotFound"
+}
+</script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
