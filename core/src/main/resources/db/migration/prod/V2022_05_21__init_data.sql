ae7362811f05bf9fb9e06b9b4e52a05c88fecac7
Тихонов Александр Дмитриевич <a.tikhonov95@gmail.com>
2022-05-12 22:12:12 +0300

    Change init data

--- core/src/main/resources/db/migration/prod/V2022_05_21__init_data.sql
+++ core/src/main/resources/db/migration/prod/V2022_05_21__init_data.sql
@@ -1,8 +1,8 @@
 INSERT INTO USERS (NAME, EMAIL)
-VALUES ('Ivan Ivanov', 'ivanov@mail.ru'),
-       ('First User', 'u1@mail.ru'),
-       ('Second User', 'u2@mail.ru'),
-       ('Ivan Petrov', 'petrov@mail.ru');
+VALUES ('Jack White', 'jack@white.ru'),
+       ('John Doe', 'john@doe.ru'),
+       ('Vincent Vega', 'vincent@vega.ru'),
+       ('Jack Black', 'jack@black.ru');
 
 INSERT INTO TASKS(TITLE, DESCRIPTION, CREATED_BY, ASSIGNEE_ID, STATUS, PRIORITY)
 VALUES ('First task title', 'First task description', 1, 2, 0, 0),
@@ -14,6 +14,6 @@ VALUES ('First task title', 'First task description', 1, 2, 0, 0),
 INSERT INTO COMMENTS(CONTENT, CREATED_AT, TASK_ID, USER_ID)
 VALUES ('Some comment 1', CURRENT_TIMESTAMP, 1, 2),
        ('Some comment 2', CURRENT_TIMESTAMP, 2, 1),
-       ('Some comment 3', CURRENT_TIMESTAMP, 2, 3),
-       ('Some comment 4', CURRENT_TIMESTAMP, 3, 3),
-       ('Some comment 5', CURRENT_TIMESTAMP, 4, 1);
+       ('Some comment 3', CURRENT_TIMESTAMP, 2, 2),
+       ('Some comment 4', CURRENT_TIMESTAMP, 3, 2),
+       ('Some comment 5', CURRENT_TIMESTAMP, 3, 1);
